e958d26d6f3cb110c0a1c2258f57bdb53d976d92
theCrowsLullaby <theCrowsLullaby@gmail.com>
2024-01-03 19:32:06 +0100

    added backbone (database)

---
+++ WriterEvent/Models/Domain/Story.cs
@@ -0,0 +1,15 @@
+﻿namespace WriterEvent.Tables.Domain
+{
+    public class Story
+    {
+        public Guid Id { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Username { get; set; }
+        public string Warnings { get; set; }
+        public string Characters { get; set; }
+        public string Description { get; set; }
+        public string Rating { get; set; }
+        public bool Age { get; set; }
+        public bool Visible { get; set; }
+    }
+}
---
+++ WriterEvent/Models/ViewModels/AddStory.cs
@@ -0,0 +1,15 @@
+﻿namespace WriterEvent.Models.ViewModels
+{
+    public class AddStory
+    {
+        public string? Heading { get; set; }
+        public string? PageTitle { get; set; }
+        public string? Content { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? FeaturedUrl { get; set; }
+        public string? UrlHandle { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string? Author { get; set; }
+        public bool Visible { get; set; }
+    }
+}

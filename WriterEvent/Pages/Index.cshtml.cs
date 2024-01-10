788cbcdb6ac4f7bee8c354ea64287fbee8fe5824
theCrowsLullaby <theCrowsLullaby@gmail.com>
2024-01-10 12:16:11 +0100

    css changes

--- WriterEvent/Pages/Index.cshtml.cs
+++ WriterEvent/Pages/Index.cshtml.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Timers;
+using Microsoft.AspNetCore.Components;
+using Timer = System.Threading.Timer;
 
 namespace WriterEvent.Pages
 {
     public class IndexModel : PageModel
     {
+        public readonly Timer timer;
+        public int days, hours, minutes, seconds;
+        private DateTime signUpDate;
+
         private readonly ILogger<IndexModel> _logger;
+
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -14,7 +23,19 @@ namespace WriterEvent.Pages
 
         public void OnGet()
         {
+            signUpDate = new DateTime(2024, 02, 01);
+            Countdown();
+            
+            
+        }
 
+        private void Countdown()
+        {
+            var distance = signUpDate - DateTime.Now;
+            days = distance.Days;
+            hours = distance.Hours;
+            minutes = distance.Minutes;
+            seconds = distance.Seconds;
         }
     }
 }
